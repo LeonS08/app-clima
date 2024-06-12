0bb9b32f0a6f293ea0747fd94ec7de1d13f01bac
Leonardo <leonardo.pedraza255@gmail.com>
2024-06-12 12:04:23 -0300

    arreglo de base de datos

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -19,17 +19,17 @@ mongoose.connect(MONGO_URI, {
   console.log("Conectado a MongoDB")
 }).catch((error) => {
   console.error("Error al conectar a MongoDB:", error)
-});
+})
 
-const esquemaBusqueda = new mongoose.Schema({
+const esquemaHistorial = new mongoose.Schema({
   ciudad: String,
   fecha: { type: Date, default: Date.now },
 })
 
-const Busqueda = mongoose.model('Busqueda', esquemaBusqueda)
+const Historial = mongoose.model('Historial', esquemaHistorial)
 
 app.get('/api/clima', async (req, res) => {
-  const ciudad = req.query.ciudad;
+  const ciudad = req.query.ciudad
   if (!ciudad) {
     return res.status(400).json({ error: 'Debe proporcionar una ciudad' })
   }
@@ -37,16 +37,29 @@ app.get('/api/clima', async (req, res) => {
   try {
     const respuesta = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${CLIMA_API_KEY}&units=metric`
-    )
+    );
     const datosClima = respuesta.data
 
-    const nuevaBusqueda = new Busqueda({ ciudad })
+    const nuevaBusqueda = new Historial({ ciudad })
     await nuevaBusqueda.save()
+
+    const historialGuardado = await Historial.findOne({ ciudad }).sort({ fecha: -1 }).exec()
+    console.log('Historial guardado:', historialGuardado)
 
     res.json(datosClima)
   } catch (error) {
     console.error('Error al obtener los datos del clima:', error.message)
     res.status(500).json({ error: 'Error al obtener los datos del clima' })
+  }
+})
+
+app.get('/api/historial', async (req, res) => {
+  try {
+    const historial = await Historial.find().sort({ fecha: -1 }).exec()
+    res.json(historial)
+  } catch (error) {
+    console.error('Error al obtener el historial:', error.message)
+    res.status(500).json({ error: 'Error al obtener el historial' })
   }
 })
 
